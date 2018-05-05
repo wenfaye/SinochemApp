99b4f91640bba2317c998bb63f11ecf32ccb799d
wenfaye <thesim4@126.com>
2018-05-05 16:20:34 +0800

    0

---
+++ bundleSource/sell/pages/Sell1Page3.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import {
+	StyleSheet,
+	Text,
+	View,
+	Image,
+	Platform,
+	Navigator,
+	TouchableHighlight,
+	TouchableOpacity,
+	TouchableWithoutFeedback,
+	Dimensions,
+} from "react-native";
+
+const ScreenWidth = Dimensions.get('window').width;
+const ScreenHeight = Dimensions.get('window').height;
+const scale = ScreenWidth / 375;
+
+const pic = require('./img/snippets.png');
+
+export class SellPage3 extends Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {};
+	}
+
+	render() {
+		<View>
+			<Text>Page3</Text>
+			<TouchableOpacity activeOpacity={0.5}
+				style={styles.lightsOpacityStyle}
+				onPress={() => {
+					//方法
+				}}>>
+				<Text>Page3Btn</Text>
+			</TouchableOpacity>
+		</View>
+	}
+
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		// justifyContent: 'center',
+		// alignItems: 'center',
+		backgroundColor: '#fff',
+	},
+	lightsOpacityStyle: {
+		height: 54,
+		width: 48,
+		justifyContent: 'center',
+		alignItems: 'center'
+	},
+});
---
+++ bundleSource/sell/pages/SinochemSellStart.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+
+import Sell1Page0 from './Sell1Page0'
+import Sell1Page1 from './Sell1Page1'
+import Sell1Page2 from './Sell1Page2'
+import Sell1Page3 from './Sell1Page3'
+import Sell1Page4 from './Sell1Page4'
+import { StackNavigator } from 'react-navigation'
